f18d47632e68952e93a061d08d71e9aa40635bed
Lydia <lmwright95@outlook.com>
2022-01-25 00:55:07 +0000

    Add styling to canvas and add create ball drawing

---
+++ README.md
@@ -0,0 +1,2 @@
+# Breakout!
+This game was built using the [Mozilla 2D game guide](https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript).
--- assets/lib/js/script.js
+++ assets/lib/js/script.js
@@ -1,8 +1,24 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+
+// Draws the ball on canvas
+function draw() {
+
+}
+
+setInterval(draw, 10);
+
+
+// Ball drawing
 ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
+ctx.arc(50, 50, 10, 0, Math.PI*2);
+ctx.fillStyle = "#1ECB9E";
 ctx.fill();
 ctx.closePath();
+
+
+// Coordinate variables
+var x = canvas.width/2;
+var y = canvas.height-30;
+
