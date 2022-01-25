a17fb06565e2375d9c335ace0e4800640b63c37b
Lydia <lmwright95@outlook.com>
2022-01-25 01:26:25 +0000

    Add footer and clean up styling

--- assets/lib/js/script.js
+++ assets/lib/js/script.js
@@ -5,7 +5,7 @@ var ctx = canvas.getContext("2d");
 // Draws the ball on canvas
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, 7, 0, Math.PI*2);
     ctx.fillStyle = "#C7DD3A";
     ctx.fill();
     ctx.closePath();
