b25fc024e298c8f03465ad661fae58135d8095b4
Lydia <lmwright95@outlook.com>
2022-01-25 00:57:19 +0000

    Move ball drawing inside draw() function

--- assets/lib/js/script.js
+++ assets/lib/js/script.js
@@ -4,18 +4,14 @@ var ctx = canvas.getContext("2d");
 
 // Draws the ball on canvas
 function draw() {
-
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#C7DD3A";
+    ctx.fill();
+    ctx.closePath();
 }
 
 setInterval(draw, 10);
-
-
-// Ball drawing
-ctx.beginPath();
-ctx.arc(50, 50, 10, 0, Math.PI*2);
-ctx.fillStyle = "#1ECB9E";
-ctx.fill();
-ctx.closePath();
 
 
 // Coordinate variables
