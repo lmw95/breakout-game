d6381b378022666f5173d891cd7e8188750bf8b9
Lydia <lmwright95@outlook.com>
2022-01-25 01:30:34 +0000

    Add functionality to allow ball to bounce off walls

--- assets/lib/js/script.js
+++ assets/lib/js/script.js
@@ -5,7 +5,7 @@ var ctx = canvas.getContext("2d");
 // Draws the ball on canvas
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 7, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#C7DD3A";
     ctx.fill();
     ctx.closePath();
@@ -14,6 +14,14 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    
     x += dx;
     y += dy;
 }
@@ -29,6 +37,10 @@ var y = canvas.height-30;
 // Coordinates to make ball move
 var dx = 2;
 var dy = -2;
+
+
+// Detecting ball bouncing off walls
+var ballRadius = 10;
 
 
 
