fe8e6fecda18449f5386547361bda83c01593df1
Lydia <lmwright95@outlook.com>
2022-01-25 01:43:43 +0000

    Add functionality that allows paddle to be moved by user's keyboard

--- assets/lib/js/script.js
+++ assets/lib/js/script.js
@@ -1,5 +1,26 @@
+// Canvas
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+// Coordinate variables
+var x = canvas.width/2;
+var y = canvas.height-30;
+
+// Coordinates to make ball move
+var dx = 2;
+var dy = -2;
+
+// Detecting ball bouncing off walls
+var ballRadius = 10;
+
+// Create the paddle
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth) / 2;
+
+// Paddle control variables
+var rightPressed = false;
+var leftPressed = false;
 
 
 // Draws the ball on canvas
@@ -11,9 +32,20 @@ function drawBall() {
     ctx.closePath();
 }
 
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#C7DD3A";
+    ctx.fill();
+    ctx.closePath();
+}
+
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -21,26 +53,47 @@ function draw() {
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
-    
+
+    if(rightPressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if(leftPressed) {
+        paddleX -= 7;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
+    }
+
     x += dx;
     y += dy;
 }
 
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+
 setInterval(draw, 10);
-
-
-// Coordinate variables
-var x = canvas.width/2;
-var y = canvas.height-30;
-
-
-// Coordinates to make ball move
-var dx = 2;
-var dy = -2;
-
-
-// Detecting ball bouncing off walls
-var ballRadius = 10;
 
 
 
