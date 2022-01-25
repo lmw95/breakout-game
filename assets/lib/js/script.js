9bc7e10f1630eadae77b68120797b5efaf467fab
Lydia <lmwright95@outlook.com>
2022-01-25 10:04:25 +0000

    Add functionality that detects ball collision with canvas wall

--- assets/lib/js/script.js
+++ assets/lib/js/script.js
@@ -1,28 +1,25 @@
-// Canvas
+//Canvas
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-// Coordinate variables
-var x = canvas.width/2;
-var y = canvas.height-30;
-
-// Coordinates to make ball move
+// Ball
+var ballRadius = 10;
+// Canva wall coordinates
+var x = canvas.width / 2;
+var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 
-// Detecting ball bouncing off walls
-var ballRadius = 10;
-
-// Create the paddle
+// Paddle
 var paddleHeight = 10;
 var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth) / 2;
+var paddleX = (canvas.width - paddleWidth) / 2;
 
-// Paddle control variables
+// User controls when gane starts
 var rightPressed = false;
 var leftPressed = false;
 
-// Paddles
+// Bricks
 var brickRowCount = 3;
 var brickColumnCount = 5;
 var brickWidth = 75;
@@ -32,66 +29,109 @@ var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 
 var bricks = [];
-for(var c=0; c<brickColumnCount; c++) {
+for (var c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
-    for(var r=0; r<brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0 };
+    for (var r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
 
 
-// Draws the ball on canvas
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#1ECE9E";
-    ctx.fill();
-    ctx.closePath();
+// Handling user keyboard controls
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+
+function keyDownHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
 }
 
+function keyUpHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
 
-function drawPaddle() {
+// Detecting ball collision with canvas wall
+function collisionDetection() {
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
+            if (b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                }
+            }
+        }
+    }
+}
+
+// Draw ball
+function drawBall() {
     ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#C7DD3A";
     ctx.fill();
     ctx.closePath();
 }
 
+// Draw paddle
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#C7DD3A";
+    ctx.fill();
+    ctx.closePath();
+}
 
+// Draw bricks
 function drawBricks() {
-    for(var c=0; c<brickColumnCount; c++) {
-        for(var r=0; r<brickRowCount; r++) {
-            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#C7DD3A";
-            ctx.fill();
-            ctx.closePath();
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status == 1) {
+                var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#C7DD3A";
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
 }
 
 
+// Calls draw functions to print to canvas
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
-    drawBricks();
+    collisionDetection();
 
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-
-    if(y + dy < ballRadius) {
+    if (y + dy < ballRadius) {
         dy = -dy;
     }
-    else if(y + dy > canvas.height-ballRadius) {
-        if(x > paddleX && x < paddleX + paddleWidth) {
-            dy = -dy;
+    else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            if (y = y - paddleHeight) {
+                dy = -dy;
+            }
         }
         else {
             alert("GAME OVER");
@@ -100,45 +140,18 @@ function draw() {
         }
     }
 
-    if(rightPressed) {
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
         paddleX += 7;
-        if (paddleX + paddleWidth > canvas.width){
-            paddleX = canvas.width - paddleWidth;
-        }
     }
-    else if(leftPressed) {
+    else if (leftPressed && paddleX > 0) {
         paddleX -= 7;
-        if (paddleX < 0){
-            paddleX = 0;
-        }
     }
 
     x += dx;
     y += dy;
 }
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
-function keyDownHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = true;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = true;
-    }
-}
-
-function keyUpHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = false;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = false;
-    }
-}
-
-
+// Set interval for canvas draw
 var interval = setInterval(draw, 10);
 
 
